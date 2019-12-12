89a5c0d85f17b98b8051259af11168d0d049d9f8
nayakar <tl@gmail.com>
2019-12-12 13:29:28 +0530

    this bank

---
+++ SbiBank/src/com/tcs/bank/Bank.java
@@ -0,0 +1,15 @@
+package com.tcs.bank;
+
+public class Bank {
+
+	public Bank() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("this is bank");
+		// TODO Auto-generated method stub
+
+	}
+
+}
